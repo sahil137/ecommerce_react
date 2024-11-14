fc8800dcb3341e870afd03953a81f81bc544710f
sahil <sahilsaini137@gmail.com>
2024-11-14 15:59:25 +0530

    initalize react router and tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routesArray } from "./utils/routes";
 
 function App() {
+  const router = createBrowserRouter(routesArray);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <RouterProvider router={router} />
+    </React.Fragment>
   );
 }
 
---
+++ src/utils/routes.js
@@ -0,0 +1,18 @@
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Home from "../pages/Home";
+
+export const routesArray = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+];
